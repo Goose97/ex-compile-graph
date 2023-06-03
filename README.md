bd9e9ec232a18d8e744e4a92c5dfec8cd1d963d5
goose <purification1997@gmail.com>
2023-06-03 14:22:38 +0700

    [Chore] publish to hex.pm

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ExCompileGraph
 
-**TODO: Add description**
+An interactive web interface to explore mix xref graph output
 
 ## Installation
 
@@ -15,7 +15,13 @@ def deps do
 end
 ```
 
+Install the package, run command and open link in the browser:
+
+```shell
+mix deps.get
+mix ex_compile_graph.server
+```
+
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at <https://hexdocs.pm/ex_compile_graph>.
-
--- mix.exs
+++ mix.exs
@@ -8,7 +8,13 @@ defmodule ExCompileGraph.MixProject do
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
-      deps: deps()
+      deps: deps(),
+      description: "Providing a web interface to interact with mix xref graph output",
+      package: [
+        exclude_patterns: ["lib/fixtures", "lib/ex_compile_graph_web/assets/node_modules"],
+        licenses: ["Apache-2.0"],
+        links: %{"Github" => "https://github.com/Goose97/ex-compile-graph"}
+      ]
     ]
   end
 
@@ -29,7 +35,8 @@ defmodule ExCompileGraph.MixProject do
       {:cowboy, "~> 2.0"},
       {:plug, "~> 1.14"},
       {:plug_cowboy, "~> 2.4"},
-      {:jason, "~> 1.0"}
+      {:jason, "~> 1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
